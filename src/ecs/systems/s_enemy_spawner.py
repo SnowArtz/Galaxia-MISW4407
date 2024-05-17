9451095ab42007a65b0925f2e1fab82926567df6
David Santiago Vargas Prada <78177400+sanvarp@users.noreply.github.com>
2024-05-16 21:00:24 -0500

    Aumenta el nivel (#37)
    Se implementó el cambio de escena cada vez que elimina a todos los
    enemigos, también se actualiza el contador de nivel con las banderas.
    
    
    https://github.com/SnowArtz/Galaxia-MISW4407/assets/78177400/752c1a47-8231-4eb8-98bd-2a5189b15d04

--- src/ecs/systems/s_enemy_spawner.py
+++ src/ecs/systems/s_enemy_spawner.py
@@ -4,7 +4,7 @@ from src.create.prefab_creator import create_enemy, create_sprite
 from src.ecs.components.c_cooldown import CCooldown
 from src.ecs.components.c_enemy_spawner import CEnemySpawner
 
-def system_enemy_spawner(world, config_enemy, config_enemies_list):
+def system_enemy_spawner(world, config_enemy, config_enemies_list,self):
     rows = config_enemies_list['rows']
     columns = config_enemies_list['columns']
 
@@ -13,6 +13,7 @@ def system_enemy_spawner(world, config_enemy, config_enemies_list):
             continue
         for event in spawner.spawn_events:
             if event not in spawner.spawned_events:
+                self.enemies_initialized=True
                 enemy_details = config_enemy[event['enemy_type']]
                 y_position = rows[str(event['row'])]
                 x_position = columns[str(event['column'])]
